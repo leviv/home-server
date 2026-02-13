19746b0db9e8ba4e201a2ea35dd6cc861319051e
Levi Villarreal <villarreallevi@gmail.com>
2026-02-13 18:38:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/api', (req, res) => {
   res.send('hello world!');
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
